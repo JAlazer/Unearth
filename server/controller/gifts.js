b1e672396eda24925cc9f24f8cc6a87f2ae8fc3f
jalazer <almanzar.j@northeastern.edu>
2026-06-24 18:03:20 -0400

    step3: fixed up accessing a specific gift

--- server/controller/gifts.js
+++ server/controller/gifts.js
@@ -1,6 +1,6 @@
 import { pool } from "../config/database.js";
 
-export default async function getGifts(req, res) {
+export async function getGifts(req, res) {
 
     const giftQuery = `
         SELECT * FROM gifts ORDER BY id ASC
@@ -11,5 +11,21 @@ export default async function getGifts(req, res) {
         res.status(200).json(giftDataResponse.rows)
     } catch (err) {
         res.status(409).json({error: err.message})
+    }
+}
+
+export async function getGiftById(req, res) {
+    try {
+        const selectQuery = `
+            SELECT name, pricePoint, audience, image, description, submittedBy, submittedOn
+            FROM gifts
+            WHERE id=${req.params.giftId}
+        `
+        const result = await pool.query(selectQuery);
+
+        console.log(`The query result: ${result.rows[0]}`);
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        res.status(409).json( { error: err.message} )
     }
 }
--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,20 +1,12 @@
 import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
 import giftData from '../data/gifts.js'
-import getGifts from '../controller/gifts.js'
-
-const __filename = fileURLToPath(import.meta.url) // whole file path to gifts.js
-const __dirname = path.dirname(__filename) // gifts
-
+import { getGifts, getGiftById } from '../controller/gifts.js'
 
 const router = express.Router()
 
 
 router.get('/', getGifts)
 
-router.get('/:giftId', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
-})
+router.get('/:giftId', getGiftById)
 
 export default router
